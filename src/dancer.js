817d0214dfb17d79862be368b09e4d746c243d08
Papatiara <shirazipatricia@gmail.com>
2018-05-01 10:20:32 -0700

    Randomized item border-colors

--- src/dancer.js
+++ src/dancer.js
@@ -6,11 +6,18 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722'];
 };
 
-Dancer.prototype.setColor = function() {
+Dancer.prototype.randomizeColor = function() {
   let min = Math.ceil(0);
   let max = Math.floor( this.colors.length );
   let seed = Math.floor(Math.random() * (max - min)) + min;
-  this.$node.css('background-color', this.colors[seed]);
+  return seed;
+};
+
+Dancer.prototype.setColor = function() {
+  this.$node.css({
+    backgroundColor: this.colors[ this.randomizeColor() ],
+    borderColor: this.colors[ this.randomizeColor() ]
+  });
 };
 
 Dancer.prototype.setPosition = function() {
@@ -23,4 +30,8 @@ Dancer.prototype.setPosition = function() {
 
 Dancer.prototype.step = function() {
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+Dancer.prototype.lineUp = function() {
+  console.log('Line up!!');
 };
