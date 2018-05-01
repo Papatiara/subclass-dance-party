a78b1c478a2d5c186c8d714086e4696e5442aca2
Papatiara <shirazipatricia@gmail.com>
2018-04-30 20:42:23 -0700

    Added the "sambando en masse!" button

--- src/init.js
+++ src/init.js
@@ -14,7 +14,28 @@ $(document).ready(function() {
     dancer.step();
 
     $('body').append(dancer.$node);
-    
+
+
+    if ( event.target = $('#masse') ) {
+      let bacchanalianFiends = 0;
+      while ( bacchanalianFiends < 100 ) {
+        bacchanalianFiends++;
+
+        let dancer = new window[danceClass](
+          $('body').height() * Math.random(),
+          $('body').width() * Math.random(),
+          Math.random() * 1000
+        );
+        
+        dancer.setColor();
+        dancer.setPosition();
+        dancer.step();
+
+        $('body').append(dancer.$node);
+      }
+    }
+
+
   });
 
 });
