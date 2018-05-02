2b4f0d3831d3ea2d0c3d7af77d69dbb7cc67eb9e
Papatiara <shirazipatricia@gmail.com>
2018-05-01 17:07:51 -0700

    nearest neighbors now share a background strobe

--- src/dancer.js
+++ src/dancer.js
@@ -15,34 +15,6 @@ Dancer.prototype.init = function() {
   this.step();
 
   $('body').append(this.$node);
-
-  if ( dancers.length >= 2 ) {
-    for ( let i = 0; i < dancers.length; i++ ) {
-      let hero = dancers[i];
-      let population = dancers.filter( function( dancer ) {
-        return dancer !== dancers[i];
-      });
-      for ( let j = 0; j < population.length; j++ ) {
-        let dancer = population[j];
-        let distanceToHero = Math.sqrt( Math.pow((hero.top - dancer.top), 2) + Math.pow(( hero.left - dancer.left), 2) );
-
-        if ( !hero.nearestNeighbor.is || hero.nearestNeighbor.distance > distanceToHero ) {
-          hero.nearestNeighbor.is = dancer;
-          hero.nearestNeighbor.distance = distanceToHero;
-          dancer.nearestNeighbor.is = hero;
-          dancer.nearestNeighbor.distance = distanceToHero;
-        }
-      }
-    }
-    if ( this.nearestNeighbor.distance === this.nearestNeighbor.is.distance ) {
-      
-      // DO SOMETHING!!
-      
-      // var teamColor = this.colors[ this.randomizeColor() ];
-      // this.$node.css('background-color', teamColor);
-      // this.nearestNeighbor.is.$node.css('background-color', teamColor);
-    }
-  }
 };
 
 
@@ -55,7 +27,7 @@ Dancer.prototype.randomizeColor = function() {
 
 Dancer.prototype.setColor = function() {
   this.$node.css({
-    backgroundColor: this.colors[ this.randomizeColor() ],
+    // backgroundColor: this.colors[ this.randomizeColor() ],
     borderColor: this.colors[ this.randomizeColor() ],
     zIndex: this.randomizeColor()
   });
@@ -73,6 +45,7 @@ Dancer.prototype.step = function() {
 
   if ( this.exists ) {
     this.setPosition();
+    this.neighbors();
 
     if ( this.top < -150 || this.top > $(window).height() + 150 || this.left < -150 || this.left > $(window).width() + 150 ) {
       this.clear();
@@ -95,6 +68,42 @@ Dancer.prototype.step = function() {
 
 };
 
+Dancer.prototype.neighbors = function() {
+  if ( dancers.length >= 2 ) {
+    for ( let i = 0; i < dancers.length; i++ ) {
+      let hero = dancers[i];
+      let population = dancers.filter( function( dancer ) {
+        return dancer !== dancers[i];
+      });
+      for ( let j = 0; j < population.length; j++ ) {
+        let dancer = population[j];
+        
+        let heroTop = hero.top + hero.$node.height()/2;
+        let heroLeft = hero.left + hero.$node.width()/2;
+
+        let dancerTop = dancer.top + dancer.$node.height()/2;
+        let dancerLeft = dancer.left + dancer.$node.width()/2;
+        
+        let distanceToHero = Math.sqrt( Math.pow((heroTop - dancerTop), 2) + Math.pow(( heroLeft - dancerLeft), 2) );
+
+        if ( !hero.nearestNeighbor.is || hero.nearestNeighbor.distance > distanceToHero ) {
+          hero.nearestNeighbor.is = dancer;
+          hero.nearestNeighbor.distance = distanceToHero;
+          dancer.nearestNeighbor.is = hero;
+          dancer.nearestNeighbor.distance = distanceToHero;
+        }
+      }
+    }
+    if ( this.nearestNeighbor.distance === this.nearestNeighbor.is.nearestNeighbor.distance && this.nearestNeighbor.is.exists ) {
+      var teamColor = this.colors[ this.randomizeColor() ];
+      this.$node.css('background-color', teamColor);
+      this.nearestNeighbor.is.$node.css('background-color', teamColor);
+    } else {
+      this.$node.css('background-color', 'transparent');
+    }
+  }
+};
+
 Dancer.prototype.lineUp = function() {
   if ( this.linedUp ) {
     this.linedUp = false;
@@ -107,5 +116,8 @@ Dancer.prototype.lineUp = function() {
 Dancer.prototype.clear = function() {
   this.exists = false;
   this.$node.remove();
+  window.dancers = dancers.filter( function( dancer ) {
+    return dancer.exists;
+  });
 };
 
