c974e702cc6823078335ea723fc4f7d4171c4d57
Papatiara <shirazipatricia@gmail.com>
2018-05-01 19:14:13 -0700

    updated the spec to include some simple tests

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,16 +1,32 @@
 describe('blinkyDancer', function() {
 
-  var blinkyDancer, clock;
+  var blinkyDancer, sambaDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    // blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
     blinkyDancer = new BlinkingDancer(10, 20, timeBetweenSteps);
+    sambaDancer = new SambaDancer(10, 20, timeBetweenSteps);
+  });
+  
+  it('samba butterflies should have a jQuery $node object', function() {
+    expect(sambaDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  
+  it('samba butterflies should be fierce!', function() {
+    expect(sambaDancer.sambaFerocity).to.equal(64);
   });
 
   it('should have a jQuery $node object', function() {
     expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  
+  it('should have a "clear" method', function() {
+    expect(sambaDancer).to.have.property('clear');
+  });
+
+  it('should have a "closest neighbor" method', function() {
+    expect(sambaDancer).to.have.property('neighbors');
   });
 
   it('should have a step function that makes its node blink', function() {
